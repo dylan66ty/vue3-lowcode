116ab0f5e44c8d4b24b23f21a66e6a44365c8c4e
dylan66ty <dylan66ty@users.noreply.github.com>
2023-08-30 12:50:39 +0000

    Update preview 5a692b7b39181ebee12905fc203df1b7c64348c5

---
+++ js/4746.24d9f54b.js
@@ -0,0 +1,8 @@
+"use strict";(self["webpackChunklowcode"]=self["webpackChunklowcode"]||[]).push([[4746],{4746:function(e,t,n){n.r(t),n.d(t,{conf:function(){return o},language:function(){return s}});
+/*!-----------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.32.1(29a273516805a852aa8edc5e05059f119b13eff0)
+ * Released under the MIT license
+ * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
+ *-----------------------------------------------------------------------------*/
+var o={comments:{lineComment:"#"}},s={defaultToken:"keyword",ignoreCase:!0,tokenPostfix:".azcli",str:/[^#\s]/,tokenizer:{root:[{include:"@comment"},[/\s-+@str*\s*/,{cases:{"@eos":{token:"key.identifier",next:"@popall"},"@default":{token:"key.identifier",next:"@type"}}}],[/^-+@str*\s*/,{cases:{"@eos":{token:"key.identifier",next:"@popall"},"@default":{token:"key.identifier",next:"@type"}}}]],type:[{include:"@comment"},[/-+@str*\s*/,{cases:{"@eos":{token:"key.identifier",next:"@popall"},"@default":"key.identifier"}}],[/@str+\s*/,{cases:{"@eos":{token:"string",next:"@popall"},"@default":"string"}}]],comment:[[/#.*$/,{cases:{"@eos":{token:"comment",next:"@popall"}}}]]}}}}]);
